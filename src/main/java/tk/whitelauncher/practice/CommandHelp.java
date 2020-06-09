1c2240ec5ca892abb9eecf98b897a9644325f684
WhiteLauncherDev <66062710+WhiteLauncherDev@users.noreply.github.com>
2020-06-09 14:18:43 +0200

    Loulou30140 -- Adding

--- src/main/java/tk/whitelauncher/practice/CommandHelp.java
+++ src/main/java/tk/whitelauncher/practice/CommandHelp.java
@@ -4,11 +4,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+/*
+    @author Loulou30140
+    @copyright Loulou30140. DON'T STEAL CODE or RENAME PLUGIN & AUTHOR
+ */
 public class CommandHelp implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
+
             Player player = (Player) sender;
             player.sendMessage("§f§l------------------");
             player.sendMessage("§bMenu d'aide: \n Commandes : \n /help, /aide, /? : afficher cette liste \n /plugins : affiche les plugins \n /duel : Fight des gens !");
--- src/main/java/tk/whitelauncher/practice/Practice.java
+++ src/main/java/tk/whitelauncher/practice/Practice.java
@@ -1,19 +1,88 @@
 package tk.whitelauncher.practice;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
 
 public class Practice extends JavaPlugin {
 
+    private Map<Player, Player> duel = new HashMap<>();
 
     @Override
     public void onEnable() {
-
+        getLogger().log(Level.ALL, "Plugin ready");
+        getCommand("duel").setExecutor(this);
         getCommand("help").setExecutor(new CommandHelp());
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+            if (label.equalsIgnoreCase("duel") && sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length == 0) {
+
+                    player.sendMessage("Aide : /duel <joueur> \n /duel <accept/deny>");
+                    return true;
+                }
+
+                if (args.length == 1) {
+                    String targetName = args[0];
+                    if (args[0].equalsIgnoreCase("accept")) {
+                        if (duel.containsKey(player)) {
+                            player.sendMessage("Le duel se lance !");
+
+                            Player targetOne = duel.get(player);
+                            targetOne.sendMessage("Le duel se lance !");
+
+                            player.teleport(new Location(Bukkit.getWorld("world"), 40, 100, 50));
+                            targetOne.teleport(new Location(Bukkit.getWorld("world"), 40, 100, 50));
+
+                            duel.remove(player);
+                        }
+                    } else if (args[0].equalsIgnoreCase("deny")) {
+                        if (duel.containsKey(player)) {
+                            Player targetOne = duel.get(player);
+                            player.sendMessage("Vous avez refusé le duel ! (espèce de lâche !)");
+                            targetOne.sendMessage("Le joueur " + player.getName() + " a refusé votre duel ce lâche !");
+
+                            duel.remove(player);
+
+                        }
+                    } else if (Bukkit.getPlayer(targetName) != null) {
+
+                        Player target = Bukkit.getPlayer(targetName);
+
+                        if (duel.containsKey(target)) {
+                            player.sendMessage("Ce joueur a déjà une demande  de duel !");
+                            return true;
+                        }
+                        duel.put(target, player);
+                        player.sendMessage("Vous venez de provoquer en duel §c" + targetName + " §r!");
+                        target.sendMessage("Vous venez de recevoir une demande de Duel de §c" + player.getName() + "§r !");
+
+
+                    } else {
+
+
+                        player.sendMessage("§cLe joueur " + targetName + " est hors ligne !");
+
+                    }
+                    return true;
+                }
+
+            }
+
+        return false;
     }
 }
